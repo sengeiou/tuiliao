3189de4f229df5c74fd7f5820162f57d2eb967e1
alucard263096 <alucard263096@126.com>
2019-09-27 19:43:48 +0800

    tt

--- source/src/app/tab2/tab2.page.ts
+++ source/src/app/tab2/tab2.page.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { AppBase } from '../AppBase';
 import { Router } from '@angular/router';
 import {  ActivatedRoute, Params } from '@angular/router';
-import { NavController, ModalController, ToastController, AlertController, NavParams,IonSlides,IonSlide } from '@ionic/angular';
+import { NavController, ModalController, ToastController, AlertController, NavParams,IonSlides,IonSlide  } from '@ionic/angular';
 import { DomSanitizer } from '@angular/platform-browser';
 import { InstApi } from 'src/providers/inst.api';
 import { ProjectApi } from 'src/providers/project.api';
@@ -30,22 +30,8 @@ export class Tab2Page extends AppBase {
       
   }
 
-  // @ViewChild(IonSlides) slides: IonSlides;
+  @ViewChild('slide01',{static:true}) IonSlides; 
  
-
-  // autoPlay() {
-  //   this.slides.startAutoplay();
-  // }
-
-  // swipeEvent(e){
-  //   this.autoPlay();
-  // }
-
-
-  // ionViewWillLeave() {
-  //   this.slides.stopAutoplay();
-  // }
-
 
   onMyLoad(){
     //参数
@@ -54,30 +40,41 @@ export class Tab2Page extends AppBase {
 
   footlist = null
   teams = []
-  rec_time = ''
+  rec_time = null
   isshow = false
+
   onMyShow(){
 
     // this.autoPlay()
 
     this.projectApi.footlist({}).then((footlist:any)=>{
       console.log(footlist)
-      this.footlist = footlist
-      var reco_time = ''
-      for(let i=0;i<this.footlist.length;i++){
-        
-        if(this.footlist[i].recom_time > reco_time){
-          reco_time = this.footlist[i].recom_time
-        }
-      }
+      // this.footlist = footlist
 
-      this.footlist = this.footlist.filter((item)=>{
-        return reco_time == item.recom_time
+      let day = new Date()
+      let year = day.getFullYear()
+      let  month :any = (day.getMonth()+1)
+      let date :any = day.getDate()
+
+      month = month < 10 ? '0'+ month : month
+      date = date < 10  ? '0'+ date : date
+      // this.nowTime = year+ "-" + month + "-" + date;
+
+      // this.rec_time = year+ "-" + month + "-" + date;
+      this.rec_time = '2019-09-26'
+      this.footlist = footlist.filter((item)=>{
+        console.log(item)
+        item.new = 'Y'
+        if(item.com_date.length>5){
+          item.com_date.splice(5,item.com_date.length-5)
+        }
+
+        return item.recom_time > this.rec_time
       })
 
-        let index = reco_time.indexOf(' ')
-        
-        this.rec_time = reco_time.slice(0,index)
+      
+     
+      console.log(this.footlist)
 
     })
     
@@ -87,7 +84,10 @@ export class Tab2Page extends AppBase {
   watchthis(list){
     console.log(list)
     this.router.navigate(['newfootdetail'],{
-      queryParams: list
+      queryParams: {
+        id:list.id,
+        new: list.new
+      }
     })
   }
 
@@ -95,7 +95,7 @@ export class Tab2Page extends AppBase {
     this.isshow = false
     console.log(event)
     event.target.classList.add('new-active')
-    event.target.parentElement.parentElement.childNodes[1].childNodes[0].classList.remove('new-active')
+    event.target.parentElement.childNodes[1].classList.remove('new-active')
     
     this.onMyShow()
 
@@ -105,12 +105,11 @@ export class Tab2Page extends AppBase {
 
   oldRecom(event){
     this.isshow = true
-    this.rec_time = ''
-    this.footlist = []
     this.teams = []
+    this.footlist = []
     console.log(event)
     event.target.classList.add('new-active')
-    event.target.parentElement.parentElement.childNodes[0].childNodes[0].classList.remove('new-active')
+    event.target.parentElement.childNodes[0].classList.remove('new-active')
 
     this.projectApi.footlist({}).then((footlist:any)=>{
       console.log(footlist)
@@ -125,43 +124,46 @@ export class Tab2Page extends AppBase {
       date = date < 10  ? '0'+ date : date
       // this.nowTime = year+ "-" + month + "-" + date;
 
-      this.nowTime = '2019-09-25'
+      // this.nowTime = '2019-09-26's
 
-      console.log(this.nowTime)
-      for(let i=0;i<footlist.length;i++){
+      this.footlist = footlist.filter((item)=>{
+        item.new = 'N'
+        console.log(item)
+        if(item.com_date.length>5){
+          item.com_date.splice(5,item.com_date.length-5)
+        }
+
         
-        let index = footlist[i].recom_time.indexOf(' ')
-        if(index != -1){
-          footlist[i].recom_time = footlist[i].recom_time.slice(0,index)
-        }else {
-          footlist[i].recom_time = footlist[i].recom_time
-        }
+        // for(let k=0;k<item.com_date.length;k++){
+        //   if(item.com_date[k].result == 'Y'){
+        //     item.com_date[k].result = '命中'
+        //   }else if(item.com_date[k].result == 'N'){
+        //     item.com_date[k].result = '未中'
+        //   }
 
-        if(footlist[i].recom_time < this.nowTime){
-          this.footlist.push(footlist[i])
-          for(let j=0;j<footlist[i].com_time.length;j++){
+        //   if(item.com_date[k].result2 == 'Y'){
+        //     item.com_date[k].result2 = '命中'
+        //   }else if(item.com_date[k].result2 == 'N'){
+        //     item.com_date[k].result2 = '未中'
+        //   }
+        // }
 
-            for(let z=0;z<footlist[i].com_time[j].footteam.length;z++){
-              this.teams.push(footlist[i].com_time[j].footteam[z])
-            }
-            
-          }
 
-        }
+        return item.recom_time <= this.rec_time
+      })
 
-      }
+      
 
-      for(let k=0;k<this.teams.length;k++){
-        if(this.teams[k].result == 'Y'){
-          this.teams[k].result = '命中'
-        }else if(this.teams[k].result == 'N'){
-          this.teams[k].result = '未中'
-        }
-      }
+      // for(let k=0;k<this.teams.length;k++){
+      //   if(this.teams[k].result == 'Y'){
+      //     this.teams[k].result = '命中'
+      //   }else if(this.teams[k].result == 'N'){
+      //     this.teams[k].result = '未中'
+      //   }
+      // }
 
 
       console.log(this.footlist)
-      console.log(this.teams)
     })
 
   }
